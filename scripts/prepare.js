d1f3092f28289837f509aa59c54a000da29ee6d2
qiangqiang-id <59910163+qiangqiang-id@users.noreply.github.com>
2023-06-18 12:46:58 +0800

    fix: 🐛 使用npm

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,16 +1,5 @@
-// import fs from 'fs';
-// import path from 'path';
 import { spawnSync } from 'child_process';
 
 spawnSync('npx husky install');
 
 spawnSync('git config core.hooksPath .husky');
-
-// const root = process.cwd();
-// const configExamplePath = path.resolve(root, './config/config.example.json');
-// const configPath = path.resolve(root, './config/config.json');
-
-// if (!fs.existsSync(configPath)) {
-//   fs.copyFileSync(configExamplePath, configPath);
-//   console.log('[prepare]创建`config/config.json`配置文件完毕');
-// }
