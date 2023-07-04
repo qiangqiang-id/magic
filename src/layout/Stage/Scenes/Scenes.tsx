c27c303985ccc75255ca970b662fe8d58a7baeb0
qiangqiang-id <59910163+qiangqiang-id@users.noreply.github.com>
2023-07-04 22:21:37 +0800

    feat: 🎸 增加场景渲染

--- src/layout/Stage/Scenes/Scenes.tsx
+++ src/layout/Stage/Scenes/Scenes.tsx
@@ -1,5 +1,40 @@
+import { observer } from 'mobx-react';
+import { useStores } from '@/store';
+import SceneStruc from '@/models/SceneStruc';
+import Renderer from '@/components/Renderer';
 import Style from './Scenes.module.less';
 
-export default function Scenes() {
-  return <div className={Style.scenes}>Scenes</div>;
+/**
+ * 预览场景大小
+ */
+const SIZE = 130;
+
+function Scenes() {
+  const { magic } = useStores();
+  const { scenes } = magic;
+
+  const renderScene = (scene: SceneStruc) => {
+    const { width = 0, height = 0 } = scene;
+    const rate = SIZE / Math.max(width, height);
+    const sceneStyle = {
+      width,
+      height,
+      transform: `scale(${rate})`,
+    };
+    return (
+      <div className={Style.scene} style={sceneStyle}>
+        <Renderer key={scene.id} layers={scene.layers || []} />;
+      </div>
+    );
+  };
+
+  return (
+    <div className={Style.scenes}>
+      <div className={Style.scenes_wrapper}>
+        {scenes.map(scene => renderScene(scene))}
+      </div>
+    </div>
+  );
 }
+
+export default observer(Scenes);
